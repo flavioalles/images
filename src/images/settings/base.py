02b96e07c3617f27f1f3ea46b189812146de90c8
Flavio Alles <flavio.alles@proton.me>
2024-07-16 14:21:19 -0300

    Adds basic app settings abstraction.
    For now, only used to configure database connection URL.

---
+++ src/images/settings/base.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    """
+    Configuration settings for the images API.
+
+    Attributes:
+        app_name (str): The name of the application.
+        description (str): A description of the API service.
+        database_url (str): The URL of the database.
+    """
+
+    app_name: str = "images"
+    description: str = "An API providing a service that handles image uploads."
+    database_url: str = Field(
+        ..., env="DATABASE_URL", description="Database connection URL."
+    )
