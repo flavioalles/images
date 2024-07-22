59363f2ec2ddd846d87bc6e4dddf719751bf97c4
Flavio Alles <flavio.alles@proton.me>
2024-07-22 18:15:47 -0300

    Adds list images route/endpoint.

--- tests/images/endpoints/test_image_router.py
+++ tests/images/endpoints/test_image_router.py
@@ -44,3 +44,66 @@ class TestCreateImageEndpoint:
         )
         assert response.json()["checksum"] == str(an_image.checksum)
         assert response.json()["status"] == str(an_image.status.value)
+
+
+class TestListImagesEndpoint:
+    """
+    Test class for the list images endpoint.
+    """
+
+    resource: str = "/api/list"
+
+    def test_when_list_images_is_successful(self, test_app, image_service):
+        """
+        Test case for listing images successfully.
+
+        Args:
+            test_app: The test client for the application.
+            image_service: The image service.
+
+        Returns:
+            None
+        """
+        assert image_service.session.query(Image).count() == 0
+
+        for path in [
+            "tests/images/fixtures/large.image.jpg",
+            "tests/images/fixtures/small.image.jpg",
+        ]:
+            with open(path, "rb") as image_file:
+                test_app.post(
+                    "/api/submit",
+                    files={"image_file": ("image.jpg", image_file, "image/jpeg")},
+                )
+
+        response = test_app.get(self.resource)
+
+        expected_list = image_service.list()
+        expected_large_image = expected_list.query.first()
+        expected_small_image = expected_list.query.all()[-1]
+
+        assert response.status_code == 200
+        assert len(response.json()) == expected_list.query.count() == 2
+
+        large_image = response.json()[0]
+        small_image = response.json()[1]
+
+        assert large_image["id"] == str(expected_large_image.id)
+        assert large_image["created"] == expected_large_image.created.strftime(
+            "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+        assert large_image["updated"] == expected_large_image.updated.strftime(
+            "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+        assert large_image["checksum"] == expected_large_image.checksum
+        assert large_image["status"] == expected_large_image.status.value
+
+        assert small_image["id"] == str(expected_small_image.id)
+        assert small_image["created"] == expected_small_image.created.strftime(
+            "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+        assert small_image["updated"] == expected_small_image.updated.strftime(
+            "%Y-%m-%dT%H:%M:%S.%fZ"
+        )
+        assert small_image["checksum"] == expected_small_image.checksum
+        assert small_image["status"] == expected_small_image.status.value
